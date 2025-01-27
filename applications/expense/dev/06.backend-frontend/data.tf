0c591d7047c964187139af035c2edb25f76e45da
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-01-27 10:43:22 +0530

    added the frontend asg

--- applications/expense/dev/06.backend-frontend/data.tf
+++ applications/expense/dev/06.backend-frontend/data.tf
@@ -13,6 +13,9 @@ data "aws_ssm_parameter" "public_subnet_ids" {
 data "aws_ssm_parameter" "backend" {
   name  = "/${var.environment}/${var.project_name}/backend-sg-id"
 }
+data "aws_ssm_parameter" "frontend" {
+  name  = "/${var.environment}/${var.project_name}/frontend-sg-id"
+}
 data "aws_ssm_parameter" "vpc_id" {
   name  = "/${var.environment}/${var.project_name}/vpc_id"
 }
@@ -23,6 +26,24 @@ data "aws_ami" "backend" {
   filter {
     name   = "name"
     values = ["backend-*"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+data "aws_ami" "frontend" {
+  most_recent = true
+  owners      = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["frontend-*"]
   }
   filter {
     name   = "architecture"
--- applications/expense/dev/06.backend-frontend/main.tf
+++ applications/expense/dev/06.backend-frontend/main.tf
@@ -31,3 +31,34 @@ module "backend_asg"{
   component = "internal"
   zone_name = var.zone_name
 }
+
+module "frontend_asg"{
+    source = "../../../../modules/expense-asg"
+    environment = var.environment
+    project_name = var.project_name
+    common_tags = {
+    "Terraform"   = "true"
+    "Author"      = "sivaramakrishna"
+    "Environment" = "dev"
+    "Project"     = "expense"
+    "Component"   = "frontend-asg"
+  }
+  ami_id = data.aws_ami.frontend.id
+  instance_type = var.instance_type
+  key_name = var.key_name
+  vpc_security_group_ids = [data.aws_ssm_parameter.frontend.value]
+  health_check_grace_period = var.health_check_grace_period
+
+  port = 80
+  health_check_path = "/health"
+  vpc_id = local.vpc_id
+  asg_max_size = var.asg_max_size
+  asg_min_size = var.asg_min_size
+  desired_capacity = var.desired_capacity
+  subnets = split(",",data.aws_ssm_parameter.public_subnet_ids.value)
+  target_value = var.target_value
+
+  listener_arn = data.aws_ssm_parameter.https_listner.value
+  component = "external"
+  zone_name = var.zone_name
+}
