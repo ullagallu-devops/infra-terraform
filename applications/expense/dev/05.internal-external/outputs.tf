62af1a9ffeffb7fbdda9c475078a34f5d1cb0a16
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-01-28 10:41:32 +0530

    made the changes

--- applications/expense/dev/05.internal-external/outputs.tf
+++ applications/expense/dev/05.internal-external/outputs.tf
@@ -1,6 +1,10 @@
 output "internal_http_listner_arn" {
   value = module.expense_internal.http_listner
 }
+
+output "external_http_listner_arn" {
+  value = module.expense_external.http_listner
+}
 output "external_https_listner_arn" {
   value = module.expense_external.https_listner
 }
