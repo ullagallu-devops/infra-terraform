3abab86d8078a528842d134ff46b48695859b5a3
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-01-28 10:35:57 +0530

    made the changes

--- applications/expense/dev/05.internal-external/paramters.tf
+++ applications/expense/dev/05.internal-external/paramters.tf
@@ -1,15 +1,18 @@
 resource "aws_ssm_parameter" "internal_http_listner" {
-  name  = "/${var.environment}/${var.project_name}/http-listner"
+  name  = "/${var.environment}/${var.project_name}/internal-http-listner"
   type  = "StringList"
   value = join(",", module.expense_internal.http_listner)
+  overwrite = true
 }
 resource "aws_ssm_parameter" "external_http_listner" {
-  name  = "/${var.environment}/${var.project_name}/https-listner"
+  name  = "/${var.environment}/${var.project_name}/external-http-listner"
   type  = "StringList"
   value = join(",", module.expense_external.http_listner)
+  overwrite = true
 }
 resource "aws_ssm_parameter" "external_https_listner" {
-  name  = "/${var.environment}/${var.project_name}/https-listner"
+  name  = "/${var.environment}/${var.project_name}/external-https-listner"
   type  = "StringList"
   value = join(",", module.expense_external.https_listner)
+  overwrite = true
 }
