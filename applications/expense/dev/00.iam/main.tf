765ddb9d1c4050ded2470199eae6802ae7bf1f89
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-01-30 15:40:32 +0530

    made the changes

--- applications/expense/dev/00.iam/main.tf
+++ applications/expense/dev/00.iam/main.tf
@@ -1,10 +1,37 @@
-locals{
-    name = "${var.environment}-${var.project_name}-${var.cw_name}"
+locals {
+  name = "${var.environment}-${var.project_name}-${var.cw_name}"
 }
+
+# Define the IAM Role with the assume role policy
 resource "aws_iam_role" "test_role" {
   name = local.name
 
   assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+  tags = merge(
+    {
+      Name = local.name
+    },
+    var.common_tags
+  )
+}
+
+# Define the IAM Policy for CloudWatch and Logs access
+resource "aws_iam_policy" "test_policy" {
+  name        = local.name
+  description = "Allow CloudWatch and Logs access"
+
+  policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
@@ -26,10 +53,17 @@ resource "aws_iam_role" "test_role" {
       }
     ]
   })
+  
   tags = merge(
     {
       Name = local.name
     },
     var.common_tags
-    )
+  )
+}
+
+# Attach the IAM Policy to the Role
+resource "aws_iam_role_policy_attachment" "test_attachment" {
+  role       = aws_iam_role.test_role.name
+  policy_arn = aws_iam_policy.test_policy.arn
 }
