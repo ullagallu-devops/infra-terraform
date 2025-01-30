76b247298a30f73bd26786e63499026c29cf1142
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-01-30 15:24:31 +0530

    update the iam

--- applications/expense/dev/00.iam/main.tf
+++ applications/expense/dev/00.iam/main.tf
@@ -2,36 +2,34 @@ locals{
     name = "${var.environment}-${var.project_name}-${var.cw_name}"
 }
 resource "aws_iam_role" "test_role" {
-  name = local.name
-  assume_role_policy = jsonencode({
-    {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents",
-        "logs:DescribeLogStreams"
-      ],
-      "Resource": "arn:aws:logs:us-east-1:522814728660:*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "cloudwatch:PutMetricData"
-      ],
-      "Resource": "*"
-    }
-  ]
-}
-  })
+  name = "test-role"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogStreams"
+        ]
+        Resource = "arn:aws:logs:us-east-1:522814728660:*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "cloudwatch:PutMetricData"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
   tags = merge(
     {
-        Name = local.name
+      Name = local.name
     },
     var.common_tags
-  )
+    )
 }
