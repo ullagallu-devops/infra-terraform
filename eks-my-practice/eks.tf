10428b363d02baa453a24e7c508d4d8d63f4fc83
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-02-26 15:49:13 +0530

    update the eks

--- eks-my-practice/eks.tf
+++ eks-my-practice/eks.tf
@@ -1,6 +1,6 @@
 module "eks"{
     depends_on = [ "module.eks_vpc"]
-    source = ../modules/eks-practice
+    source = "../modules/eks-practice"
     subnets = module.eks_vpc.public_subnet_ids
     endpoint_private_access = false
     endpoint_public_access = true
