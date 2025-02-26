0f0bcd5ddf1742ab6c8222d21ffb00e66730f879
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-02-27 00:26:58 +0530

    creating the cluster with full capacity

--- eks-my-practice/eks.tf
+++ eks-my-practice/eks.tf
@@ -17,9 +17,9 @@ module "eks"{
         blue = {
             instance_types = ["t3a.medium"]
             capacity_type = "SPOT"
-            desired_size = 2
-            max_size = 2
-            min_size = 2
+            desired_size = 6
+            max_size = 10
+            min_size = 6
         }
     }
 
