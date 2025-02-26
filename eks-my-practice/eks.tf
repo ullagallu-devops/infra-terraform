7a1fec126cda1316db3e5f74df338cfcce7282c4
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-02-26 16:12:43 +0530

    update the code

--- eks-my-practice/eks.tf
+++ eks-my-practice/eks.tf
@@ -1,7 +1,7 @@
 module "eks"{
     depends_on = [module.eks_vpc]
     source = "../modules/eks-practice"
-    environment = var.environemnt
+    environment = var.environment
     project_name = var.project_name
     vpc_id = module.eks_vpc.vpc_id
     subnets = module.eks_vpc.public_subnet_ids
