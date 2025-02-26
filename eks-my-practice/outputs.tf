e48d714f9b7a3afd25bf5dc9321faa64746b6038
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-02-26 15:05:01 +0530

    add the outputs

--- eks-my-practice/outputs.tf
+++ eks-my-practice/outputs.tf
@@ -1,3 +1,15 @@
 output "vpc_id"{
     value = module.eks_vpc.vpc_id
 }
+output "public_subnet_ids"{
+    value = module.eks_vpc.public_subnet_ids
+}
+output "private_subnet_ids"{
+    value = module.eks_vpc.private_subnet_ids
+}
+output "db_subnet_ids"{
+    value = module.eks_vpc.db_subnet_ids
+}
+output "db_subnet_group"{
+    value = module.eks_vpc.db_subnet_group
+}
