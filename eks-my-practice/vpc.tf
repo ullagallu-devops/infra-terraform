b1f4b9e1e79d276653ec2920a6bd9de0d442d220
siva <sivaram0434@gmail.com>
2025-02-26 14:55:30 +0530

    add the vpc module

--- eks-my-practice/vpc.tf
+++ eks-my-practice/vpc.tf
@@ -1,20 +1,17 @@
 module "eks_vpc" {
-  source                     = "../modules/vpc"
-  vpc_cidr                   = "192.168.0.0/16"
-  environment                = var.environment
-  project_name               = var.project_name
-  azs                        = ["ap-south-1a", "ap-south-1b"]
-  public_subnet_cidrs        = ["192.168.1.0/24", "192.168.2.0/24"]
-  private_subnet_cidrs       = ["192.168.11.0/24", "192.168.12.0/24"]
-  database_subnet_cidrs      = ["192.168.21.0/24", "192.168.22.0/24"]
-  nat_gateway_configuration  = "none"
-  enable_vpc_flow_logs       = true
-  enable_kubernetes_k8s_tags = true
+  source = "../modules/vpc-advanced"
+  vpc_cidr = "192.168.0.0/16"
+  environment = var.environment
+  project_name = var.project_name
   common_tags = {
-    Author = "Sivaramakrishna"
     Terraform = true
-    Project= var.project_name
-    Environment= var.environment
-
+    Developer = "siva"
   }
+  azs = ["ap-south-1a", "ap-south-1b"]
+  public_subnet_cidrs = ["192.168.1.0/24","192.168.2.0/24"]
+  private_subnet_cidrs = ["192.168.3.0/24","192.168.4.0/24"]
+  database_subnet_cidrs = ["192.168.5.0/24","192.168.6.0/24"]
+  enable_kubernetes_k8s_tags = true
+  nat_gateway_configuration = "none"
+  enable_vpc_flow_logs = false
 }
