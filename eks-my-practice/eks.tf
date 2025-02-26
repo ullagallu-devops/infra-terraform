37eefb4a846605c9f85b8498a323db2aa943944e
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-02-26 16:07:25 +0530

    update the configuaration

--- eks-my-practice/eks.tf
+++ eks-my-practice/eks.tf
@@ -1,11 +1,11 @@
-module "eks"{
-    depends_on = [module.eks_vpc]
-    source = "../modules/eks-practice"
-    environment = var.environemnt
-    project_name = var.project_name
-    vpc_id = module.eks_vpc.vpc_id
-    subnets = module.eks_vpc.public_subnet_ids
-    endpoint_private_access = false
-    endpoint_public_access = true
-    public_access_cidrs = ["0.0.0.0/0"]
-}
+# module "eks"{
+#     depends_on = [module.eks_vpc]
+#     source = "../modules/eks-practice"
+#     environment = var.environemnt
+#     project_name = var.project_name
+#     vpc_id = module.eks_vpc.vpc_id
+#     subnets = module.eks_vpc.public_subnet_ids
+#     endpoint_private_access = false
+#     endpoint_public_access = true
+#     public_access_cidrs = ["0.0.0.0/0"]
+# }
--- modules/eks-practice/variables.tf
+++ modules/eks-practice/variables.tf
@@ -3,3 +3,4 @@ variable "project_name"{}
 variable "subnets"{}
 variable "endpoint_private_access"{}
 variable "endpoint_public_access"{}
+variable "vpc_id" {}
