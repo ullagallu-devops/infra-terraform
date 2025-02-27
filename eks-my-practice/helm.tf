fac1f3988b1255fe0f78aec71492fd70b36112b7
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-02-27 17:10:38 +0530

    update the config

--- eks-my-practice/helm.tf
+++ eks-my-practice/helm.tf
@@ -1,9 +1,11 @@
 resource "null_resource" "kube-bootstrap" {
-  depends_on = [module.eks]  
+  depends_on = [module.eks.cluster_id]  
   
   provisioner "local-exec" {
     command = <<EOF
-    aws eks update-kubeconfig --region ${var.region} --name ${module.eks.cluster_name}
+    aws eks update-kubeconfig \
+      --region ${var.region} \
+      --name ${module.eks.cluster_name}
 EOF
   }
 }
