4ea99cf908fd22adb9b43105a1b46f7cf798299d
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-02-27 17:19:38 +0530

    update

--- eks-my-practice/helm.tf
+++ eks-my-practice/helm.tf
@@ -1,10 +1,6 @@
 resource "null_resource" "kube-bootstrap" {
   depends_on = [module.eks]
 
-  triggers = {
-    cluster_name = module.eks.cluster_name
-  }
-
   provisioner "local-exec" {
     command = <<EOF
 aws eks update-kubeconfig --name ${module.eks.cluster_name}
