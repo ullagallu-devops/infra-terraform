ece82740d5931eb00ba8f30ce6d52777021e6587
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-02-27 17:24:33 +0530

    update

--- eks-my-practice/helm.tf
+++ eks-my-practice/helm.tf
@@ -2,9 +2,6 @@ resource "null_resource" "kube-bootstrap" {
   depends_on = [module.eks]
 
   provisioner "local-exec" {
-    command = <<EOF
-aws eks update-kubeconfig --name "$(echo ${module.eks.cluster_name} | tr -d '[:space:]')"
-EOF
-    interpreter = ["/bin/bash", "-c"]
+    command = "aws eks update-kubeconfig --name ${module.eks.cluster_name}"
   }
 }
