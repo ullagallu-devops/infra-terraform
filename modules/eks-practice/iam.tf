e59edd9994a64701644484d7544970a763c98a54
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-02-27 07:54:05 +0530

    added the policy to provision volumes

--- modules/eks-practice/iam.tf
+++ modules/eks-practice/iam.tf
@@ -54,3 +54,197 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
   role       = aws_iam_role.example.name
 }
 
+resource "aws_iam_policy" "policy" {
+  name        = "${local.name}-EBS-CSI-Policy"
+  description = "This is EBS CSI Driver to Provision EBS volumes on behalf of EKS"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:DescribeAvailabilityZones",
+          "ec2:DescribeInstances",
+          "ec2:DescribeSnapshots",
+          "ec2:DescribeTags",
+          "ec2:DescribeVolumes",
+          "ec2:DescribeVolumesModifications"
+        ],
+        "Resource" : "*"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:CreateSnapshot",
+          "ec2:ModifyVolume"
+        ],
+        "Resource" : "arn:aws:ec2:*:*:volume/*"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:AttachVolume",
+          "ec2:DetachVolume"
+        ],
+        "Resource" : [
+          "arn:aws:ec2:*:*:volume/*",
+          "arn:aws:ec2:*:*:instance/*"
+        ]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:CreateVolume",
+          "ec2:EnableFastSnapshotRestores"
+        ],
+        "Resource" : "arn:aws:ec2:*:*:snapshot/*"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:CreateTags"
+        ],
+        "Resource" : [
+          "arn:aws:ec2:*:*:volume/*",
+          "arn:aws:ec2:*:*:snapshot/*"
+        ],
+        "Condition" : {
+          "StringEquals" : {
+            "ec2:CreateAction" : [
+              "CreateVolume",
+              "CreateSnapshot"
+            ]
+          }
+        }
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:DeleteTags"
+        ],
+        "Resource" : [
+          "arn:aws:ec2:*:*:volume/*",
+          "arn:aws:ec2:*:*:snapshot/*"
+        ]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:CreateVolume"
+        ],
+        "Resource" : "arn:aws:ec2:*:*:volume/*",
+        "Condition" : {
+          "StringLike" : {
+            "aws:RequestTag/ebs.csi.aws.com/cluster" : "true"
+          }
+        }
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:CreateVolume"
+        ],
+        "Resource" : "arn:aws:ec2:*:*:volume/*",
+        "Condition" : {
+          "StringLike" : {
+            "aws:RequestTag/CSIVolumeName" : "*"
+          }
+        }
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:DeleteVolume"
+        ],
+        "Resource" : "arn:aws:ec2:*:*:volume/*",
+        "Condition" : {
+          "StringLike" : {
+            "ec2:ResourceTag/ebs.csi.aws.com/cluster" : "true"
+          }
+        }
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:DeleteVolume"
+        ],
+        "Resource" : "arn:aws:ec2:*:*:volume/*",
+        "Condition" : {
+          "StringLike" : {
+            "ec2:ResourceTag/CSIVolumeName" : "*"
+          }
+        }
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:DeleteVolume"
+        ],
+        "Resource" : "arn:aws:ec2:*:*:volume/*",
+        "Condition" : {
+          "StringLike" : {
+            "ec2:ResourceTag/kubernetes.io/created-for/pvc/name" : "*"
+          }
+        }
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:CreateSnapshot"
+        ],
+        "Resource" : "arn:aws:ec2:*:*:snapshot/*",
+        "Condition" : {
+          "StringLike" : {
+            "aws:RequestTag/CSIVolumeSnapshotName" : "*"
+          }
+        }
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:CreateSnapshot"
+        ],
+        "Resource" : "arn:aws:ec2:*:*:snapshot/*",
+        "Condition" : {
+          "StringLike" : {
+            "aws:RequestTag/ebs.csi.aws.com/cluster" : "true"
+          }
+        }
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:DeleteSnapshot"
+        ],
+        "Resource" : "arn:aws:ec2:*:*:snapshot/*",
+        "Condition" : {
+          "StringLike" : {
+            "ec2:ResourceTag/CSIVolumeSnapshotName" : "*"
+          }
+        }
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:DeleteSnapshot"
+        ],
+        "Resource" : "arn:aws:ec2:*:*:snapshot/*",
+        "Condition" : {
+          "StringLike" : {
+            "ec2:ResourceTag/ebs.csi.aws.com/cluster" : "true"
+          }
+        }
+      }
+    ]
+
+  })
+}
+
+
+resource "aws_iam_policy_attachment" "test-attach" {
+  name       = "EBS-CSI-DRIVER-Attachment"
+  roles      = [aws_iam_role.example.name]
+  policy_arn = aws_iam_policy.policy.arn
+}
+
