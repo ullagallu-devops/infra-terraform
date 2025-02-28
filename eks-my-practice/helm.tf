adceafbf5ae5d3dfa9651c2fd7a4b96ee1a4c9f4
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-02-28 16:30:40 +0530

    added the helm chart

--- eks-my-practice/helm.tf
+++ eks-my-practice/helm.tf
@@ -1,7 +1,21 @@
-# resource "null_resource" "kube-bootstrap" {
-#   depends_on = [module.eks]
+resource "null_resource" "kube-bootstrap" {
+  depends_on = [module.eks]
 
-#   provisioner "local-exec" {
-#     command = "aws eks update-kubeconfig --name ${module.eks.cluster_name}"
-#   }
-# }
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --name ${module.eks.cluster_name}"
+  }
+}
+
+resource "helm_release" "ebs_csi_driver" {
+  name       = "aws-ebs-csi-driver"
+  repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
+  chart      = "aws-ebs-csi-driver"
+  namespace  = "kube-system"
+  create_namespace = false
+
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+}
+
