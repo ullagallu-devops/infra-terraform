0167714f98460fc538d673627bef3ddfdebc9eb8
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-02-28 16:38:55 +0530

    update the null resournce

--- eks-my-practice/helm.tf
+++ eks-my-practice/helm.tf
@@ -2,7 +2,9 @@ resource "null_resource" "kube-bootstrap" {
   depends_on = [module.eks]
 
   provisioner "local-exec" {
-    command = "aws eks update-kubeconfig --name ${module.eks.cluster_name}"
+    command = <<EOF
+    aws eks update-kubeconfig  --name ${var.env}-eks
+    EOF
   }
 }
 
