496b6f32c27d50bcc19bec3412c88fbaa7f6d06a
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-02-28 17:11:17 +0530

    update the kubeconfig

--- eks-my-practice/helm.tf
+++ eks-my-practice/helm.tf
@@ -2,7 +2,7 @@ resource "null_resource" "kube-bootstrap" {
   depends_on = [module.eks]
 
   provisioner "local-exec" {
-    command = aws eks update-kubeconfig --name ${module.eks.cluster_name}
+    command = "aws eks update-kubeconfig --name ${module.eks.cluster_name}"
   }
 }
 
