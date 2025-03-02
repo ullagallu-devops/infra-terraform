3777cf130488cd746639946b0f930781599e6057
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-03-02 11:32:41 +0530

    remove ns creation

--- eks-my-practice/helm.tf
+++ eks-my-practice/helm.tf
@@ -7,18 +7,18 @@ resource "null_resource" "kube-bootstrap" {
     command = "aws eks update-kubeconfig --region ap-south-1 --name ${module.eks.cluster_name}"
   }
 }
-resource "null_resource" "kube-ns" {
-  # triggers = {
-  #   always_run = timestamp()
-  # }
+# resource "null_resource" "kube-ns" {
+#   # triggers = {
+#   #   always_run = timestamp()
+#   # }
 
-  provisioner "local-exec" {
-    command = <<EOT
-      kubectl create ns expense || true
-      kubectl create ns instana || true
-    EOT
-  }
-}
+#   provisioner "local-exec" {
+#     command = <<EOT
+#       kubectl create ns expense || true
+#       kubectl create ns instana || true
+#     EOT
+#   }
+# }
 resource "helm_release" "ebs_csi_driver" {
   depends_on = [null_resource.kube-bootstrap]
   name       = "aws-ebs-csi-driver"
