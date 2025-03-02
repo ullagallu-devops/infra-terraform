b75f6e87d86da46b9d0eb6d19cd944ff2af08585
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-03-02 19:25:34 +0530

    update the config

--- eks-my-practice/helm.tf
+++ eks-my-practice/helm.tf
@@ -7,18 +7,7 @@ resource "null_resource" "kube-bootstrap" {
     command = "aws eks update-kubeconfig --region ap-south-1 --name ${module.eks.cluster_name}"
   }
 }
-# resource "null_resource" "kube-ns" {
-#   # triggers = {
-#   #   always_run = timestamp()
-#   # }
 
-#   provisioner "local-exec" {
-#     command = <<EOT
-#       kubectl create ns expense || true
-#       kubectl create ns instana || true
-#     EOT
-#   }
-# }
 resource "helm_release" "ebs_csi_driver" {
   depends_on = [null_resource.kube-bootstrap]
   name       = "aws-ebs-csi-driver"
@@ -28,27 +17,27 @@ resource "helm_release" "ebs_csi_driver" {
   create_namespace = false
 }
 
-resource "helm_release" "alb_ingress_controller" {
-  depends_on = [null_resource.kube-bootstrap]
+# resource "helm_release" "alb_ingress_controller" {
+#   depends_on = [null_resource.kube-bootstrap]
 
-  name       = "aws-load-balancer-controller"
-  repository = "https://aws.github.io/eks-charts"
-  chart      = "aws-load-balancer-controller"
-  namespace  = "aws-load-balancer-controller"
-  create_namespace = true
+#   name       = "aws-load-balancer-controller"
+#   repository = "https://aws.github.io/eks-charts"
+#   chart      = "aws-load-balancer-controller"
+#   namespace  = "aws-load-balancer-controller"
+#   create_namespace = true
 
-  set {
-    name  = "clusterName"
-    value = module.eks.cluster_name
-  }
+#   set {
+#     name  = "clusterName"
+#     value = module.eks.cluster_name
+#   }
   
-  set {
-    name  = "serviceAccount.create"
-    value = "false"
-  }
+#   set {
+#     name  = "serviceAccount.create"
+#     value = "false"
+#   }
 
-  set {
-    name  = "serviceAccount.name"
-    value = "aws-load-balancer-controller-sa"
-  }
-}
+#   set {
+#     name  = "serviceAccount.name"
+#     value = "aws-load-balancer-controller-sa"
+#   }
+# }
