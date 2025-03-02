cdf481a8c4f0f9f8f50a641f07bd72e0d4d970ad
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-03-02 19:13:46 +0530

    update the code

--- eks-my-practice/helm.tf
+++ eks-my-practice/helm.tf
@@ -28,27 +28,27 @@ resource "helm_release" "ebs_csi_driver" {
   create_namespace = false
 }
 
-# resource "helm_release" "alb_ingress_controller" {
-#   depends_on = [null_resource.kube-bootstrap]
+resource "helm_release" "alb_ingress_controller" {
+  depends_on = [null_resource.kube-bootstrap]
 
-#   name       = "aws-load-balancer-controller"
-#   repository = "https://aws.github.io/eks-charts"
-#   chart      = "aws-load-balancer-controller"
-#   namespace  = "aws-load-balancer-controller"
-#   create_namespace = true
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "aws-load-balancer-controller"
+  create_namespace = true
 
-#   set {
-#     name  = "clusterName"
-#     value = module.eks.cluster_name
-#   }
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_name
+  }
   
-#   set {
-#     name  = "serviceAccount.create"
-#     value = "false"
-#   }
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
 
-#   set {
-#     name  = "serviceAccount.name"
-#     value = "aws-load-balancer-controller-sa"
-#   }
-# }
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller-sa"
+  }
+}
